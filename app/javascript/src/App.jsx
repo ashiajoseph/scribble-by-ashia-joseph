6a6fa2aa316dfe194af0e2495e221097ec17cf7d
ashiajoseph <ashiajoseph@gmail.com>
2021-12-01 20:06:28 +0530

    Added axios.js

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { initializeLogger } from "common/logger";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  /*eslint no-undef: "off"*/
-  initializeLogger();
-  }, []);    
+  useEffect(() => {
+    /*eslint no-undef: "off"*/
+    initializeLogger();
+    
+  }, []);
+
+
+
   return (
     <Router>
       <Switch>

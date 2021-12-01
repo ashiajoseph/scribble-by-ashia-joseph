f8286cb06c4a8b48b5620845e49b6ee6c55fabf2
ashiajoseph <ashiajoseph@gmail.com>
2021-12-01 19:21:54 +0530

    Added js-logger

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect} from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { initializeLogger } from "common/logger";
 
 const App = () => {
+
+useEffect(() => {
+  /*eslint no-undef: "off"*/
+  initializeLogger();
+  }, []);    
   return (
     <Router>
       <Switch>

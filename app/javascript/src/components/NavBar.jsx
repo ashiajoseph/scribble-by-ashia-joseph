45efe8ec454425aaa643c182ea3975e596482f3a
ashiajoseph <ashiajoseph@gmail.com>
2021-12-02 00:49:39 +0530

    Added styling to Navbar

--- app/javascript/src/components/NavBar.jsx
+++ app/javascript/src/components/NavBar.jsx
@@ -2,17 +2,25 @@ import React from "react";
 
 import { ExternalLink } from "@bigbinary/neeto-icons";
 import { Typography, Button } from "@bigbinary/neetoui/v2";
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+
+  const articlesTextColor =
+    pathname === "/" ? "text-indigo-500" : "text-gray-400";
+  const settingsTextColor =
+    pathname === "/settings" ? "text-indigo-500" : "text-gray-400";
   return (
-    <div>
-      <div className="flex ">
+    <div className="flex justify-between items-center px-6 py-4 border-b-2 border-nav-bottom">
+      <div className="flex items-center font-semibold	">
         <Typography style="h4">Scribble</Typography>
-        <Link to="" className="">
+        <Link to="/" className={`ml-6 ${articlesTextColor}`}>
           Articles
         </Link>
-        <Link to="" className="">
+        <Link to="/settings" className={`ml-6 ${settingsTextColor}`}>
           Settings
         </Link>
       </div>

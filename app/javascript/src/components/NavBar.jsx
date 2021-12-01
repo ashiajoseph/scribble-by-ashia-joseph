f10f1dab5d4e829d7e31438d7aee75ba36c99bd8
ashiajoseph <ashiajoseph@gmail.com>
2021-12-02 00:08:16 +0530

    Navbar - left nav links added

--- app/javascript/src/components/NavBar.jsx
+++ app/javascript/src/components/NavBar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import { Typography } from "@bigbinary/neetoui/v2";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <div>
+        <Typography style="h4">Scribble</Typography>
+        <Link to="" className="">
+          Articles
+        </Link>
+        <Link to="" className="">
+          Settings
+        </Link>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default NavBar;

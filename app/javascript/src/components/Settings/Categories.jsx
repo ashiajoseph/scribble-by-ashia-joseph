c40ac5cc494629cdbc223ccf52e26eb6ae3476ac
ashiajoseph <ashiajoseph@gmail.com>
2021-12-02 13:49:28 +0530

    Added add new catergory button

--- app/javascript/src/components/Settings/Categories.jsx
+++ app/javascript/src/components/Settings/Categories.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import { Plus } from "@bigbinary/neeto-icons";
+
 const Categories = () => {
-  return <div>ddd</div>;
+  const handleClick = () => {
+    //logger.info("clicked")
+  };
+  return (
+    <div>
+      <button
+        className="flex items-center text-indigo-500 font-medium text-base focus:outline-none"
+        onClick={handleClick}
+      >
+        <Plus size={18} /> Add new category
+      </button>
+    </div>
+  );
 };
 
 export default Categories;

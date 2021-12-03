e85a997851b0832a16038a11d5a0658589b42690
ashiajoseph <ashiajoseph@gmail.com>
2021-12-03 15:36:58 +0530

    MenuItem styling modified

--- app/javascript/src/components/Settings/MenuBar/MenuItem.jsx
+++ app/javascript/src/components/Settings/MenuBar/MenuItem.jsx
@@ -10,18 +10,21 @@ const MenuBar = ({
   setSelectedSettingOption,
   children,
 }) => {
-  const selectedStyle = selectedSettingOption === title ? "bg-indigo-50" : null;
+  const selectedBackgroundStyle =
+    selectedSettingOption === title ? "bg-indigo-50" : null;
+  const selectedIconColor =
+    selectedSettingOption === title ? "text-black" : "neeto-ui-text-gray-500";
   return (
     <div
-      className={`flex items-center p-2 mb-2 rounded-sm hover:bg-gray-200 ${selectedStyle}`}
+      className={`flex items-center p-2 mb-2 rounded-sm hover:bg-gray-200 ${selectedBackgroundStyle}`}
       onClick={() => setSelectedSettingOption(title)}
     >
-      {children}
+      <div className={selectedIconColor}>{children}</div>
       <div className="ml-3">
         <Typography style="h4" className="font-medium		">
           {title}
         </Typography>
-        <Typography style="h6" className="neeto-ui-text-gray-500 font-normal">
+        <Typography style="h6" className={` font-normal ${selectedIconColor}`}>
           {description}
         </Typography>
       </div>

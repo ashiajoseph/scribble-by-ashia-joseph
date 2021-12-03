775ff60e8fe92040c20204428bf39c82a4027fbb
ashiajoseph <ashiajoseph@gmail.com>
2021-12-03 08:56:15 +0530

    Added update to categoriesApi

--- app/javascript/src/apis/categories.js
+++ app/javascript/src/apis/categories.js
@@ -4,10 +4,12 @@ const create = payload => axios.post("/categories", payload);
 const list = () => axios.get("/categories");
 const reorder_position = ({ id, payload }) =>
   axios.put(`/categories/${id}/reorder_position`, payload);
+const update = ({ id, payload }) => axios.put(`/categories/${id}`, payload);
 
 const categoriesApi = {
   create,
   list,
   reorder_position,
+  update,
 };
 export default categoriesApi;

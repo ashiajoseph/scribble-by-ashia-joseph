bbe5ba961a6b2a04badbe2dc35d05e1f495cdbe2
ashiajoseph <ashiajoseph@gmail.com>
2021-12-06 22:27:05 +0530

    Added Article model validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  belongs_to :category, optional: true
+  validates :title, presence: true, length: { maximum: Constants::MAX_NAME_LENGTH }
+  validates :content, presence: true
+  enum status: { draft: "draft", published: "published" }
+
+  before_validation :set_date
+
+  private
+
+    def set_date
+      self.date = Time.zone.today
+    end
 end

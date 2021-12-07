bed0d1551f616d906824f65765fbb510822d7748
ashiajoseph <ashiajoseph@gmail.com>
2021-12-07 22:03:56 +0530

    Formatted date

--- app/javascript/src/components/Landing/Table.jsx
+++ app/javascript/src/components/Landing/Table.jsx
@@ -5,29 +5,65 @@ import { useTable } from "react-table";
 const Table = ({ tableColumnHeader, articleList }) => {
   const data = useMemo(() => articleList, []);
 
-  const columnHeader = Object.keys(tableColumnHeader)
-    .filter(column => tableColumnHeader[column])
-    .map(column => {
-      return {
-        id: column,
-        Header: column.toUpperCase(),
-        accessor: column,
-      };
-    });
+  const applyStyle = column => {
+    let style = "";
+    const cols = ["author", "category", "status"];
+    if (column === "title") style = "text-indigo-500 font-medium";
+    else if (cols.includes(column)) style = "neeto-ui-text-gray-600";
+
+    return style;
+  };
+
+  const filteredColumns = Object.keys(tableColumnHeader).filter(
+    column => tableColumnHeader[column]
+  );
+  const columnHeader = filteredColumns.map(column => {
+    return {
+      id: column,
+      Header: column.toUpperCase(),
+      accessor: column,
+      Cell: ({ row }) => (
+        <span className={applyStyle(column)}>{row.values[column]}</span>
+      ),
+    };
+  });
+
+  if (filteredColumns.length !== 0) {
+    columnHeader.push(
+      {
+        id: "delete",
+        width: 35,
+        Cell: () => (
+          <button className="focus:outline-none" onClick={() => {}}>
+            <i className="ri-delete-bin-line neeto-ui-text-gray-600 mr-3 hover:text-red-600 text-md"></i>
+          </button>
+        ),
+      },
+      {
+        id: "edit",
+        width: 40,
+        Cell: () => (
+          <button className="focus:outline-none" onClick={() => {}}>
+            <i className="ri-pencil-line neeto-ui-text-gray-600 hover:text-black  text-md"></i>
+          </button>
+        ),
+      }
+    );
+  }
   const columns = useMemo(() => columnHeader, [tableColumnHeader]);
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({ columns: columns, data: data });
 
   return (
     <table {...getTableProps()} className="w-95 mx-auto">
-      <thead className="neeto-ui-text-gray-500 text-left font-semibold ">
+      <thead className="neeto-ui-text-gray-500 text-left font-semibold">
         {headerGroups.map((headerGroup, ind) => (
           <tr key={ind} {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column, ind) => (
               <th
                 key={ind}
                 {...column.getHeaderProps()}
-                className="py-3 px-1 text-sm"
+                className="py-3 pl-3 pr-1 text-sm"
               >
                 {column.render("Header")}
               </th>
@@ -50,7 +86,7 @@ const Table = ({ tableColumnHeader, articleList }) => {
                         width: cell.column.width,
                       },
                     })}
-                    className={`capitalize break-all p-2 ${bgColor}`}
+                    className={`capitalize break-all pl-3 pr-1 py-3 ${bgColor}`}
                   >
                     {cell.render("Cell")}
                   </td>
--- app/views/categories/index.json.jbuilder
+++ app/views/categories/index.json.jbuilder
@@ -12,14 +12,16 @@ end
 
 json.article_list_with_categories @category_list do |category|
   json.array! category.articles do |article|
-    json.extract! article, :id, :title, :content, :status, :date
+    json.extract! article, :id, :title, :content, :status
+    json.date article.date.strftime("%B %e, %Y")
     json.author "Oliver Smith"
     json.category article.category.name
   end
 end
 
 json.article_list_without_categories Article.where.missing(:category) do |article|
-  json.extract! article, :id, :title, :content, :status, :date
+  json.extract! article, :id, :title, :content, :status
+  json.date article.date.strftime("%B %e, %Y")
   json.author "Oliver Smith"
   json.category "-"
 end

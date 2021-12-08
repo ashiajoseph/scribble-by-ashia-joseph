95c1695594f0860ae4627d0da8498f080598285b
ashiajoseph <ashiajoseph@gmail.com>
2021-12-08 12:49:58 +0530

    Added destroy to articlesApi

--- app/javascript/src/apis/articles.js
+++ app/javascript/src/apis/articles.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 const create = payload => axios.post("/articles", payload);
+const destroy = id => axios.delete(`/articles/${id}`);
 const show = id => axios.get(`/articles/${id}`);
 const update = ({ id, payload }) => axios.put(`/articles/${id}`, payload);
 
 const articlesApi = {
   create,
+  destroy,
   show,
   update,
 };

f576918993e06a080fb53ef03e15f323d07a3afe
ashiajoseph <ashiajoseph@gmail.com>
2021-12-09 20:15:24 +0530

    Added General component

--- app/javascript/src/components/Settings/General/index.jsx
+++ app/javascript/src/components/Settings/General/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Container from "../../Common/Container";
+import MenuBar from "../MenuBar";
+
 const General = () => {
-  return <div>jj</div>;
+  return (
+    <Container>
+      <div className="flex h-full">
+        <MenuBar />
+        <div className="w-45 mx-auto mt-12 mb-6"></div>
+      </div>
+    </Container>
+  );
 };
 
 export default General;

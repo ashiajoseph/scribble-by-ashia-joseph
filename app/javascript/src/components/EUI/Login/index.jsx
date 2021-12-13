1f4b6812b49405d521c8ffa6e48b02d28823d9bc
ashiajoseph <ashiajoseph@gmail.com>
2021-12-13 15:57:11 +0530

    Fetched website name

--- app/javascript/src/components/EUI/Login/index.jsx
+++ app/javascript/src/components/EUI/Login/index.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { PageLoader } from "@bigbinary/neetoui/v2";
+
+import websiteApi from "apis/website";
 
 import NavBar from "../NavBar";
-//import websiteApi from "apis/website";
+
 const Login = () => {
+  const [loading, setLoading] = useState(true);
+  const [websiteName, setWebsiteName] = useState("");
+  const fetchWebsiteInfo = async () => {
+    try {
+      const response = await websiteApi.show();
+      const { website } = response.data;
+      setWebsiteName(website.name);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchWebsiteInfo();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="py-10 mt-40">
+        <PageLoader />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <NavBar />
+      <NavBar name={websiteName} />
     </div>
   );
 };
--- app/javascript/src/components/EUI/NavBar.jsx
+++ app/javascript/src/components/EUI/NavBar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const NavBar = () => {
+import { Typography } from "@bigbinary/neetoui/v2";
+
+const NavBar = ({ name }) => {
   return (
-    <div className="sticky top-0 flex justify-center items-center px-6 py-4 border-b-2 border-nav-menubar bg-white h-1/6 z-10"></div>
+    <div className="sticky top-0 flex justify-center items-center px-6 py-4 border-b-2 border-nav-menubar bg-white h-1/6 z-10">
+      <Typography style="h4" weight="semibold">
+        {name}
+      </Typography>
+    </div>
   );
 };
 

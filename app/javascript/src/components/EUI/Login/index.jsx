9bd7bbf62931d4ac7869c265563b279a9e8c225a
ashiajoseph <ashiajoseph@gmail.com>
2021-12-13 16:36:40 +0530

    Added image

--- app/javascript/src/components/EUI/Login/index.jsx
+++ app/javascript/src/components/EUI/Login/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
-import { PageLoader } from "@bigbinary/neetoui/v2";
+import { PageLoader, Typography } from "@bigbinary/neetoui/v2";
+import LoginImage from "images/login.svg";
 
 import websiteApi from "apis/website";
 
@@ -34,9 +35,26 @@ const Login = () => {
   }
 
   return (
-    <div>
+    <>
       <NavBar name={websiteName} />
-    </div>
+      <div className="w-28 mx-auto">
+        <div>
+          <img src={LoginImage} className="mx-auto" />
+        </div>
+        <Typography
+          style="h2"
+          className="neeto-ui-text-gray-800 font-semibold mb-1"
+        >
+          {websiteName} is password protected!
+        </Typography>
+        <Typography
+          style="h4"
+          className="neeto-ui-text-gray-600 font-normal mb-10"
+        >
+          Enter the password to gain access to {websiteName}.
+        </Typography>
+      </div>
+    </>
   );
 };
 

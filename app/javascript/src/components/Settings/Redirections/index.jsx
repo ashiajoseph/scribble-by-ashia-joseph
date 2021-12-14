c4a4d4e1375640cd052964409e47fb4aeb438e9a
ashiajoseph <ashiajoseph@gmail.com>
2021-12-15 00:10:52 +0530

    Added heading and subheading

--- app/javascript/src/components/Settings/Redirections/index.jsx
+++ app/javascript/src/components/Settings/Redirections/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Typography } from "@bigbinary/neetoui/v2";
+
 import Container from "../../Common/Container";
 import MenuBar from "../MenuBar";
 
@@ -8,6 +10,22 @@ const Redirections = () => {
     <Container>
       <div className="flex h-full">
         <MenuBar />
+        <div className="w-45 mx-auto mt-10 mb-6">
+          <Typography
+            style="h2"
+            className="neeto-ui-text-gray-800 font-semibold mb-1"
+          >
+            Redirections
+          </Typography>
+          <Typography
+            style="h4"
+            className="neeto-ui-text-gray-600 font-normal mb-10"
+          >
+            Create and configure redirection rules to send users from old links
+            to new links. All redirections are performed with 301 status codes
+            to be SEO friendly.
+          </Typography>
+        </div>
       </div>
     </Container>
   );

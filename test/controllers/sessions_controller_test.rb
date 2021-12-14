4527fd9b177c1c8f0cfc6d04256efcfe13cc23eb
ashiajoseph <ashiajoseph@gmail.com>
2021-12-14 20:24:32 +0530

    Added test for SessionsController

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,20 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @website = create(:website)
+  end
+
+  def test_should_login_user_with_valid_credentials
+    post session_path, params: { login: { password: "welcome123" } }
+    assert_response :success
+    assert_equal response.parsed_body["authentication_token"], @website.authentication_token
+  end
+
+  def test_shouldnt_login_user_with_invalid_credentials
+    post session_path, params: { login: { password: "welcome" } }
+
+    assert_response :unauthorized
+    assert_equal response.parsed_body["error"], t("access_denied")
+  end
 end

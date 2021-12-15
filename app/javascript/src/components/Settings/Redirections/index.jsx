1529c75ba31744f6ec77dae2e63ad4a9264301e0
ashiajoseph <ashiajoseph@gmail.com>
2021-12-15 08:12:40 +0530

    Added path headers

--- app/javascript/src/components/Settings/Redirections/index.jsx
+++ app/javascript/src/components/Settings/Redirections/index.jsx
@@ -19,12 +19,34 @@ const Redirections = () => {
           </Typography>
           <Typography
             style="h4"
-            className="neeto-ui-text-gray-600 font-normal mb-10"
+            className="neeto-ui-text-gray-600 font-normal mb-8"
           >
             Create and configure redirection rules to send users from old links
             to new links. All redirections are performed with 301 status codes
             to be SEO friendly.
           </Typography>
+          <div className="py-10 px-8 bg-indigo-50">
+            <div className="flex flex-row items-center justify-around">
+              <Typography
+                style="h6"
+                className="text-gray-500 font-semibold w-2/5"
+              >
+                FROM PATH
+              </Typography>
+              <Typography
+                style="h6"
+                className="text-gray-500 font-semibold w-2/5 ml-3"
+              >
+                TO PATH
+              </Typography>
+              <Typography
+                style="h6"
+                className="text-gray-500 font-semibold w-1/5 text-center ml-2"
+              >
+                ACTIONS
+              </Typography>
+            </div>
+          </div>
         </div>
       </div>
     </Container>

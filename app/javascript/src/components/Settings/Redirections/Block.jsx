9432a58d53976d9f74b6049d1a342549d0b51fd2
ashiajoseph <ashiajoseph@gmail.com>
2021-12-15 16:57:24 +0530

    Form displayed on clicking edit icon

--- app/javascript/src/components/Settings/Redirections/Block.jsx
+++ app/javascript/src/components/Settings/Redirections/Block.jsx
@@ -1,8 +1,11 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+
+import Form from "./Form";
 
 const Block = ({ id, from, to, deleteRedirection }) => {
   const origin = window.location.origin;
   const redirectionRef = useRef();
+  const [showEditForm, setShowEditForm] = useState(false);
 
   const handleDelete = () => {
     const toBeDeleted = window.confirm(
@@ -12,26 +15,34 @@ const Block = ({ id, from, to, deleteRedirection }) => {
   };
 
   return (
-    <div
-      className="flex flex-row bg-white p-3 rounded-sm my-2 justify-between"
-      ref={redirectionRef}
-    >
-      <div className="w-38 break-words">
-        <span className="neeto-ui-text-gray-400">{origin}</span>
-        <span className="neeto-ui-text-gray-700">{from}</span>
-      </div>
-      <div className="w-38">
-        <span className="neeto-ui-text-gray-700">{origin + to}</span>
-      </div>
-      <div className="w-1/6 flex flex-row justify-evenly">
-        <button className="focus:outline-none" onClick={handleDelete}>
-          <i className="ri-delete-bin-line neeto-ui-text-gray-600 mr-3 hover:text-red-600 text-md"></i>
-        </button>
-        <button className="focus:outline-none" onClick={() => {}}>
-          <i className="ri-pencil-line neeto-ui-text-gray-600 hover:text-black  text-md"></i>
-        </button>
-      </div>
-    </div>
+    <>
+      {!showEditForm && (
+        <div
+          className="flex flex-row bg-white p-3 rounded-sm my-2 justify-between"
+          ref={redirectionRef}
+        >
+          <div className="w-38 break-words">
+            <span className="neeto-ui-text-gray-400">{origin}</span>
+            <span className="neeto-ui-text-gray-700">{from}</span>
+          </div>
+          <div className="w-38">
+            <span className="neeto-ui-text-gray-700">{origin + to}</span>
+          </div>
+          <div className="w-1/6 flex flex-row justify-evenly">
+            <button className="focus:outline-none" onClick={handleDelete}>
+              <i className="ri-delete-bin-line neeto-ui-text-gray-600 mr-3 hover:text-red-600 text-md"></i>
+            </button>
+            <button
+              className="focus:outline-none"
+              onClick={() => setShowEditForm(true)}
+            >
+              <i className="ri-pencil-line neeto-ui-text-gray-600 hover:text-black  text-md"></i>
+            </button>
+          </div>
+        </div>
+      )}
+      {showEditForm && <Form />}
+    </>
   );
 };
 
